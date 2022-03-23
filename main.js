91f708aef46b188312b905733b5fee8653ec2a09
Lilian Forger <lilianforger@gmail.com>
2022-03-23 14:40:27 +0100

    Last changes/attempts at layout and functionality

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 import "styles.css";
 
 const activityBtn = document.querySelector(".nav-activity");
-const dropdownTest = document.querySelector(".dropdown-activity");
+const dropdownTest = document.querySelector(".dropdown-activity-hidden");
 
-activityBtn.addEventListener('onmouseenter', function () {
-    if (dropdownTest.style.visibility === 'hidden') {
-      dropdownTest.style.visibility = 'visible'
-    } else {
-      dropdownTest.style.visibility = 'hidden'
-    }
+activityBtn.addEventListener("mouseover", function () { dropdownToggleOn });
+activityBtn.addEventListener("mouseout", function () { dropdownToggleOff });
+
+const dropdownToggleOn = () => dropdownTest.style.opacity="0"
+const dropdownToggleOff = () => dropdownTest.style.opacity="1"
+
+/*
+activityBtn.forEach(function(img){
+  img.addEventListener("mouseover", function(){ mOverImg(img) });
+  img.addEventListener("mouseout", function(){ mOutImg(img) });
 });
+
+function mOverImg(img) {
+  img.style.opacity="0";
+}
+
+function mOutImg(img) {
+  img.style.opacity="1";
+}
+
+*/
