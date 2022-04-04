67842ca0a938c2fcbe685ddf96937a4a7d16fc2a
Lilian Forger <lilianforger@gmail.com>
2022-04-04 08:56:01 +0200

    Fixed DOM manipulation for dropdown menu

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 const activityBtn = document.querySelector(".nav-activity");
 const dropdownTest = document.querySelector(".dropdown-activity");
 
@@ -7,20 +5,4 @@ const dropdownToggleOn = () => dropdownTest.style.visibility="visible";
 const dropdownToggleOff = () => dropdownTest.style.visibility="hidden";
 
 activityBtn.addEventListener('mouseover', dropdownToggleOn);
-activityBtn.addEventListener('mouseout', dropdownToggleOff);
-
-/*
-activityBtn.forEach(function(img){
-  img.addEventListener("mouseover", function(){ mOverImg(img) });
-  img.addEventListener("mouseout", function(){ mOutImg(img) });
-});
-
-function mOverImg(img) {
-  img.style.opacity="0";
-}
-
-function mOutImg(img) {
-  img.style.opacity="1";
-}
-
-*/
+dropdownTest.addEventListener('mouseout', dropdownToggleOff);
